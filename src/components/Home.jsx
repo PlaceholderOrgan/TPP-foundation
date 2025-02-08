0362be6d468b7b9b2e3300d833ab6659e31c7f44
Task2401 <ana.chr.mr@gmail.com>
2025-02-08 17:24:39 +0200

    malakia

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,16 +12,43 @@ function Home() {
         {/* Grid layout section with three cards */}
         <div className="section-grid">
           <div className="section-card">
-            <h3>PlaceHolder1</h3>
-            <p>Information about sanitation eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefforts.</p>
+            <h3>How does a nuclear power plant work?</h3>
+            <p>This is a short, but informative video that provides insight to how nuclear power plants work</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/TW6s2Q4Zl9E"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className="section-card">
-            <h3>PlaceHolder2</h3>
-            <p>Key problems facing river health.</p>
+            <h3> Coal 101: What's Wrong with Coal?</h3>
+            <p>This is a short but inforative video about the concerns of coal as an energy source.</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/9Wv2GKaukZU"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className="section-card">
-            <h3>PlaceHolder3</h3>
-            <p>Long-term solutions and practices.</p>
+            <h3> Renewable Energy 101 | National Geographic</h3>
+            <p>A short but informative video regarding renewable energy sources.</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/1kUE0BZtTRc"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
       </main>
